5a9b815277cb58f88682e783f2087bc1270f4c37
manhtuong0619 <mtuong669@gmail.com>
2025-11-24 21:19:15 +0700

    fix(build): privy node

--- src/shared/helper/signature.ts
+++ src/shared/helper/signature.ts
@@ -1,7 +1,3 @@
-import config from "@config/index";
-import { EIP712_DOMAIN, EIP721_TYPES } from "@shared/constant";
-import { ethers } from "ethers";
-
 export type EIP721Message = {
     address: string;
     message: string;
@@ -9,30 +5,4 @@ export type EIP721Message = {
     chainId: number;
     network: string;
     url: string;
-};
-
-export const verifyEIP721Message = (
-    message: EIP721Message,
-    signature: string,
-    signer: string,
-) => {
-    const messagePayload = {
-        Message: config.eip712.message,
-        URL: message.url,
-        Network: message.network,
-        Account: message.address,
-        Nonce: message.nonce,
-    };
-    return (
-        signer ===
-        ethers.verifyTypedData(
-            {
-                ...EIP712_DOMAIN,
-                chainId: message.chainId,
-            },
-            EIP721_TYPES,
-            messagePayload,
-            signature,
-        )
-    );
 };
