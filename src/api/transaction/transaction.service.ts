fa9f75a6c14a62652b516a9f6b39d933165421d0
Nguyen-Sy <synguyentien.work@gmail.com>
2025-11-27 12:42:02 +0700

    feat: update submit transaction return

--- src/api/transaction/transaction.service.ts
+++ src/api/transaction/transaction.service.ts
@@ -186,6 +186,7 @@ export class TransactionService extends BaseService {
         return {
             transactionId,
             stripePayment: stripePayment.id,
+            ...transaction,
         };
     }
 
